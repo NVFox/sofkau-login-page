25e4c427f62c5ec8f64895e671ac08415c55232d
Andrés Téllez <davidroa8@hotmail.com>
2025-01-20 21:00:08 -0500

    chore: add typescript config

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,30 @@ const __dirname = import.meta.dirname;
 
 export default {
     entry: {
-        client: "./src/client/index.js"       
+        client: "./src/client/index.ts"       
     },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env'],
+                            ['@babel/preset-typescript']
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
     }
 }
