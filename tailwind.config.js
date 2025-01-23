9169938e6e91caed1a9ffb1e8c541296d9fdf2d0
Andrés Téllez <davidroa8@hotmail.com>
2025-01-22 22:01:06 -0500

    style: change language for login and add animations for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@
 export default {
     content: [
         "./public/**/*.{html,js}",
-        "./src/**/*.{ejs,js}"
+        "./src/**/*.{ejs,js}",
+        "./src/client/**/*.{js,ts}"
     ],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: '0', transform: 'translateY(20px)' },
+                    '100%': { opacity: '1', transform: 'translateY(0)' },
+                },
+                fadeOut: {
+                    '0%': { opacity: '1', transform: 'translateY(0)' },
+                    '100%': { opacity: '0', transform: 'translateY(20px)' },
+                },
+            },
+            animation: {
+                fadeIn: 'fadeIn 0.5s ease-out',
+                fadeOut: 'fadeOut 0.5s ease-out',
+            },
+        },
     },
     plugins: [],
 }
